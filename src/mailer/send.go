9f8d8c45ffe7dea0f7c157d42cb40b4b90e88a66
Evan Mena <evan.mena13@gmail.com>
2018-06-17 01:52:01 +0000

    Move more credentials to bin

--- src/mailer/send.go
+++ src/mailer/send.go
@@ -9,24 +9,10 @@ import (
 )
 
 func send(body string) {
-	from, pass := auth()
-	//	to := "evan.mena13@gmail.com"
-	//	mime := "MIME-version 1.0;<br />Content-Type: text/html; charset=%quot;UTF-8%quot;<br /><br />"
-	//	subject := "Good morning! It's " + time.Now().Weekday().String() + "<br />"
-	//	msg := []byte("From: " + from + "<br />" +
-	//		"To: " + to + "<br />" +
-	//		"Subject: " + subject + "<br />" +
-	//		mime +
-	//		("<html><body>" + body + "</body></html>"))
-	//
-	//	err := smtp.SendMail("smtp.gmail.com:587", smtp.PlainAuth("", from, pass, "smtp.gmail.com"), from, []string{to}, msg)
-	//	if err != nil {
-	//		log.Printf("smtp error: %s", err)
-	//		return
-	//	}
+	from, pass, to := initialize()
 	m := gomail.NewMessage()
 	m.SetHeader("From", from)
-	m.SetHeader("To", "evan.mena13@gmail.com")
+	m.SetHeader("To", to)
 	m.SetHeader("Subject", "Good morning! It's "+time.Now().Weekday().String())
 	m.SetBody("text/html", body)
 
@@ -38,7 +24,7 @@ func send(body string) {
 	log.Print("sent")
 }
 
-func auth() (string, string) {
+func initialize() (string, string, string) {
 	f, err := os.Open("../../bin/smtp.cred")
 	if err != nil {
 		log.Fatal("Unable to read smtp credentials")
@@ -50,5 +36,7 @@ func auth() (string, string) {
 	from := scanner.Text()
 	scanner.Scan()
 	pass := scanner.Text()
-	return from, pass
+	scanner.Scan()
+	to := scanner.Text()
+	return from, pass, to
 }
